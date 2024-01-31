c1eb9a86a3641891b4fa406079716a62c6899b3a
Tainá Santana <taina.santana1305@gmail.com>
2024-01-30 23:57:22 -0300

    a caminho de uma boa grid

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const greetingElement = document.getElementById("greeting");
 
 const today = new Date();
 
-/**Verificando horário */
+/**Verificando horário*/
 
 if((today.getHours() > 4) && (today.getHours() < 12)){
     greetingElement.innerText = `Bom dia!`;
@@ -29,13 +29,13 @@ const observer = new ResizeObserver(( () => {
     const cards = document.querySelector('.cards');
     heightCards = window.getComputedStyle(cards).height; //pegando a altura do card
 
-    /**convertendo a altura para tipo numerico */
+    /**convertendo a altura para tipo numerico*/
 
     heightCards = parseInt(heightCards)
 
     const numColumns = Math.floor(containerWidth / heightCards); // pegando a quantidade de colunas dentro do container
 
-    container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(180px, 1fr))`;
+    container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(180px, 200px))`;
 }))
 
 observer.observe(container);
